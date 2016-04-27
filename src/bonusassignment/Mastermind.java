564ea767020c53beecbf89fcaeb70102f10b5995
ginsengtang <ginsengtang@bitbucket.org>
2016-04-27 13:54:15 -0500

    fixed a continue bug
    added timer and guesses to winner dialogue

--- src/bonusassignment/Mastermind.java
+++ src/bonusassignment/Mastermind.java
@@ -14,16 +14,30 @@ public class Mastermind {
 		boolean done = false;
 		System.out.println(
 				"***Welcome to Mastermind!*** \nThere is a secret code consisting of 4 pegs of different colors. \nInput a string of 4 characters based on the colors you guess:"
-						+ "\n\t'B'lue, 'G'reen, 'O'range, 'P'urple, 'R'ed, or 'Y'ellow. \nA black peg indicates one of the pegs in your guess is both the correct color and position, while a white peg indicates one of the pegs is the correct"
-						+ "color but in the wrong position. \nIf you manage to guess the secret code in 12 guesses or less, you win!");
+						+ "\n\t'B'lue, 'G'reen, 'O'range, 'P'urple, 'R'ed, or 'Y'ellow. \nA black peg indicates one of the pegs in your guess is" +
+						"\nboth the correct color and position, while a white" +
+						"\npeg indicates one of the pegs is the correct color " +
+						"\nbut in the wrong position." +
+						" \nIf you manage to guess the secret code" +
+						"\nin 12 guesses or less, you win!");
 //		System.out.println("" + code.get(0) + "" + code.get(1) + "" + code.get(2) + "" + code.get(3));
+		StopWatch stopWAtch = new StopWatch();
+		int guesses = 0;
 		for (int i = 0; i < 12 && !done; i++) {
 			System.out.println("\nGuesses remaining: " + (12 - i));
 			done = guess();
+			if (i == 0) {
+				stopWAtch.start();
+			}
+			guesses = i + 1;
 		}
 
-		if (done)
+		if (done) {
+			stopWAtch.stop();
 			System.out.println("***Congratulations, you win!***");
+			System.out.println("***You took " + stopWAtch.getElapsedTime() + " milliseconds. Try to beat this time!***");
+			System.out.println("***You took " + guesses + " guesses. Can your friends beat this?***");
+		}
 		else
 			System.out.println("Out of guesses! Maybe next time.");
 	}
@@ -61,21 +75,28 @@ public class Mastermind {
 				int blackPegs = 0;
 				int whitePegs = 0;
 				ArrayList<Character> codeTemp = (ArrayList<Character>) code.clone();
-				System.out.print("Input your guess. Choices: B, G, O, P, R, Y\n>");
+				System.out.print("Input 4 of the following characters: B, G, O, P, R, Y\n>");
 				BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 				String input = read.readLine();
 				if (input.length() != 4) {
 					System.out.println("Invalid input");
 					continue;
 				}
+				
+				boolean invalid_input = false;
 				for (int i = 0; i < 4; i++) {
 					char color = input.charAt(i);
 					if (!(color == 'B' || color == 'G' || color == 'O' || color == 'P' || color == 'R'
 							|| color == 'Y')) {
 						System.out.println("Invalid input");
-						continue;
+						invalid_input = true;
+						break;
 					}
 				}
+				if (invalid_input) {
+					continue;
+				}
+				
 				for (int i = 0; i < 4; i++) {
 					char color = input.charAt(i);
 					if (color == code.get(i)) {
